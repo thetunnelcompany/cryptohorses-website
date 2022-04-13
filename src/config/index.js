514a3385313815a95573689bf9c15500d0fef556
Nazar <nazar.khatsko@gmail.com>
2022-04-13 16:42:49 +0300

    Edit whitepaper link

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const WEBSITE_URL = "https://crypto-horses.com"
 const API_URL = "https://api.crypto-horses.com"
-const WHITE_PAPER = "https://the-tunnel-company.gitbook.io/crypto-horses-white-paper"
+const WHITE_PAPER = "https://whitepaper.crypto-horses.com"
 const TWITTER = "https://twitter.com/chorsesgame"
 const MEDIUM = "https://medium.com/@cryptohorses"
 const DISCORD = "https://discord.gg/Kmzc5cAdUb"
